10c31dec6f428e62e86dafb84d7f8433af2fbeaa
Soham Raut <sohamrautvk18@gmail.com>
2025-06-05 09:17:01 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log("this is a simple node.js application.")
+for(let i=0;i<44;i++){
+    console.log('current count : ${i}');
+}
+let string = "thus the high guard has risen";
+console.log('string length:', string.length);
+console.log("hola");

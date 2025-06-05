3b62de93223554fc96e1a1a2629bad223d5eda3d
Soham Raut <sohamrautvk18@gmail.com>
2025-06-05 10:36:45 +0530

    i learned function

---
+++ yash.js
@@ -0,0 +1,12 @@
+//const prompt = require('prompt-sync')();
+//const name = prompt("What is your name? ");
+//console.log("Hello, " + name + "!");
+
+
+const xyz = function (x=5)
+{
+    let ans =0;
+    ans=x*x;
+    return ans;
+}
+console.log("the answer is: " + xyz(9));
